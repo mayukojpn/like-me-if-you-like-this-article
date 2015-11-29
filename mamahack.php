1ac26a3ea7e3453dce3b599eb7e13e73a1e7fbb8
Mayuko Moriyama <mayuko.jpn@gmail.com>
2015-11-30 04:59:14 +0900

    Initial commit

---
+++ mamahack.php
@@ -0,0 +1,160 @@
+<?php
+/*
+Plugin Name: Facebook like if you like this article
+Description: This will recommend to like any Facebook page on the bottom of every single article.
+Author: Mayuko Moriyama
+Version: 0.1
+Author URI: http://blog.mayuko.me
+Plugin URI: https://github.com/mayukojpn/fb-like-if-you-like-this-article
+Text Domain: mamahack
+*/
+
+$mamahack = new FB_if_you_like();
+$mamahack->register();
+
+class FB_if_you_like {
+
+	public function register()
+	{
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+	}
+	public function plugins_loaded()
+	{
+		add_filter( 'the_content', array( $this, 'the_content' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+		add_action( 'wp_footer', array( $this, 'wp_footer' ), 21 );
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_filter( 'script_loader_src', array( $this, 'async_script_loader_src' ), '', 2 );
+
+
+	}
+	public function wp_enqueue_scripts()
+	{
+		/*
+		* If you want to style only your theme. You can stop style of this plugin.
+		*
+		* add_filter( 'mamahack_style', "__return_false" );
+		*/
+		$style = apply_filters( 'mamahack_style', plugins_url( 'css/mamahack.css', __FILE__ ) );
+		if ( $style ) {
+			wp_enqueue_style(
+				'mamahack_style',
+				$style
+			);
+		}
+
+	}
+	public function the_content( $contents )
+	{
+		if ( ! is_singular() ) {
+			return $contents;
+		}
+		$like = '<div style="padding:10px 0px;"></div>';
+
+		if (get_option('mamahack_fb_account'))
+		{
+			$like .= '<div class="p-entry__push">';
+			$like .= '<div class="p-entry__pushThumb" style="background-image: url(' . wp_get_attachment_url( get_post_thumbnail_id($post->ID) ). ')"></div>';
+			$like .= '<div class="p-entry__pushLike">';
+			$like .= '<p>'._('この記事が気に入ったら<br>いいね！しよう').'</p>';
+			$like .= '<div class="p-entry__pushButton">';
+			$like .= '<iframe src="https://www.facebook.com/plugins/like.php?href=https://www.facebook.com/';
+			$like .=  esc_html( get_option('mamahack_fb_account') );
+			$like .= '&send=false&layout=button_count&width=100&show_faces=false&action=like&colorscheme=light&font=arial&height=20" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>';
+			$like .= '</div>';
+			$like .= '<p class="p-entry__note">最新情報をお届けします</p>';
+			$like .= '</div>';
+			$like .= '</div>';
+		}
+		if( get_option( 'mamahack_tw_account' ) )
+		{
+			$like .= '<div class="p-entry__tw-follow">';
+			$like .= '<div class="p-entry__tw-follow__cont">';
+			$like .= '<p class="p-entry__tw-follow__item">';
+
+			if( $mamahack_tw_message = esc_html( get_option( 'mamahack_tw_message ') ) )
+			{
+				$like .= $mamahack_tw_message;
+			}
+			else
+			{
+				$like .= 'Twitterでフォローしよう！';
+			}
+			$like .= '</p>';
+			$like .= '<a href="https://twitter.com/'.esc_html( get_option('mamahack_tw_account') ).'" class="twitter-follow-button p-entry__tw-follow__item" data-show-count="false" data-size="large" data-show-screen-name="false">Follow @'.esc_html( get_option('mamahack_tw_account') ).'</a>';
+			$like .= '</div>';
+			$like .= '</div>';
+		}
+
+		return apply_filters( 'mamahack_the_content', $contents . $like, $like, $contents );
+	}
+	public function async_script_loader_src() {
+		$html = "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
+			echo $html;
+		add_action( 'wp_footer', array( $this, 'wp_footer' ), 21 );
+		return '';
+	}
+	public function wp_footer()
+	{
+		echo '<div id="fb-root"></div>';
+
+	}
+
+	public function mamahack_section_message() {
+
+	}
+
+	public function mamahack_fb_account() { ?>
+	 	https://www.facebook.com/<input name="mamahack_fb_account" id="mamahack_fb_account" type="text" size="30" value="<?php
+	 		echo esc_html( get_option('mamahack_fb_account') ); ?>">/
+		<?php
+	}
+
+	public function mamahack_tw_message() { ?>
+	 	<input name="mamahack_tw_message" id="mamahack_tw_message" type="text" size="30" value="<?php
+	 		echo esc_html( get_option('mamahack_tw_message') ); ?>">
+		<?php
+	}
+
+	public function mamahack_tw_account() { ?>
+	 	@<input name="mamahack_tw_account" id="mamahack_tw_account" type="text" size="30" value="<?php
+	 		echo esc_html( get_option('mamahack_tw_account') ); ?>">
+		<?php
+	}
+
+
+	public function admin_init() {
+		add_settings_section(
+			'mamahack',
+			_( '「この記事が良かったらいいね」の設定' ),
+			array( $this, 'mamahack_section_message' ),
+			'reading' );
+
+	 	add_settings_field(
+			'mamahack_fb_account',
+			_( 'Facebook ページ名' ),
+			array( $this, 'mamahack_fb_account' ),
+			'reading',
+			'mamahack');
+
+		add_settings_field(
+			'mamahack_tw_message',
+			_( 'Twitter メッセージ' ),
+			array( $this, 'mamahack_tw_message' ),
+			'reading',
+			'mamahack');
+
+		add_settings_field(
+			'mamahack_tw_account',
+			_( 'Twitter アカウント' ),
+			array( $this, 'mamahack_tw_account' ),
+			'reading',
+			'mamahack');
+
+
+	 	register_setting('reading','mamahack_fb_account');
+		register_setting('reading','mamahack_tw_message');
+		register_setting('reading','mamahack_tw_account');
+	 }
+
+}
---
+++ readme.txt
@@ -0,0 +1,47 @@
+=== Facebook like if you like this article ===
+
+Contributors: mayukojpn, manami1030
+Donate link: http://blog.mayuko.me
+Tags: facebook, Twitter
+Requires at least: 3.8
+Tested up to: 4.3.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+This will recommend to like any Facebook page on the bottom of every single article.
+
+== Description ==
+
+This will recommend to like any Facebook page on the bottom of every single article.
+
+== Installation ==
+
+##English
+1. Upload this folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the `Plugins` menu in WordPress.
+3. Set your Facebook and Twitter account through the `Option`>`Reading` menu in WordPress.
+
+##日本語
+1. フォルダを`/wp-content/plugins/`ディレクトリにアップロードします。
+2. 管理画面上の`プラグイン`から、プラグインを有効にします。
+3. 管理画面上の`設定`>`表示設定`から、表示させるHTMLタグを設定し、保存します。
+
+== Screenshots ==
+
+1. Facebook Like button & Twitter Follow button will show on the bottom of article.
+1. Facebook いいね & Twitter フォローボタンが記事の末尾に表示されます。
+
+2. Set your text through the `Option`>`Reading` menu in WordPress
+2. 管理画面上の`設定`>`表示設定`から、表示させるHTMLタグを設定し、保存します。
+
+== Changelog ==
+
+= 0.1 =
+
+* first version.
+
+== Upgrade Notice ==
+
+= 0.1 =
+
+* first version.
